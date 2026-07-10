d3e4bc12106c6176cf4c2823f4af36386ae661bc
coder-2011 <65727553+coder-2011@users.noreply.github.com>
2026-07-09 23:03:23 -0700

    release lambda-labs 1.0.0

--- packages/lambda-labs/setup.py
+++ packages/lambda-labs/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name="lambda-labs",
-  version="0.1.0",
+  version="1.0.0",
   description="Small CLI for Lambda Cloud.",
   py_modules=["lambda_labs"],
   install_requires=["rich"],
